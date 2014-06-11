2f7865a98649c737ba7f20144a159bbddc644f85
Marco Heumann <marco.heumann@web.de>
2014-06-11 12:23:40 +0200

    removed bug(all postions visible) and mad map bigger (full Mannheim)

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 var map = L.map('map');
+var marker, circle;
 
 L.tileLayer('img/map/{z}/{x}/{y}.jpg', {
     minZoom: 15,
@@ -6,17 +7,24 @@ L.tileLayer('img/map/{z}/{x}/{y}.jpg', {
 }).addTo(map);
 
 navigator.geolocation.getCurrentPosition(onLocationFound, onLocationError);
-navigator.geolocation.watchPosition(onLocationFound);
+navigator.geolocation.watchPosition(onLocationUpdated);
 
 map.locate({setView: true});
 
-function onLocationFound(position) {
+function onLocationFound(position) {    
     var radius = position.coords.accuracy / 2;
 
-    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+    marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-    L.circle([position.coords.latitude, position.coords.longitude], radius).addTo(map); /*49.4874359,8.4614417*/
+    circle = L.circle([position.coords.latitude, position.coords.longitude], radius).addTo(map); /*49.4874359,8.4614417*/
+}
+
+function onLocationUpdated (position) {
+    map.removeLayer(marker);
+    map.removeLayer(circle);
+    
+    onLocationFound(position);
 }
 
 function onLocationError(e) {
