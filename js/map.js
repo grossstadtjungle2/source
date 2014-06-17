bb6e4144ef57170a5afef79c5a1c95ec8e670ff8
Marco Heumann <marco.heumann@web.de>
2014-06-17 15:55:05 +0200

    removed known issue #1

--- js/map.js
+++ js/map.js
@@ -2,24 +2,45 @@ var map = L.map('map');
 var marker, circle;
 
 L.tileLayer('img/map/{z}/{x}/{y}.jpg', {
-    minZoom: 15,
+    minZoom: 18, // changed to 18 for debug because only zoom 18 exists (installls faster)
     maxZoom: 18
 }).addTo(map);
+
+var MyControl = L.Control.extend({
+    options: {
+        position: 'bottomright'
+    },
+
+    onAdd: function (map) {
+        // create the control container with a particular class name
+        var container = L.DomUtil.create('div', 'center-map-control');
+
+        L.DomUtil.create('div', 'leaflet-control center-text', container);
+        L.DomEvent.addListener(container, 'click', function() {centerMap();});
+
+        return container;
+    }
+});
+
+map.addControl(new MyControl());
+$(".center-text").text("Zentrieren");
+
+map.locate({setView: true});
 
 navigator.geolocation.getCurrentPosition(onLocationFound, onLocationError);
 navigator.geolocation.watchPosition(onLocationUpdated);
 
-map.locate({setView: true});
-
-function onLocationFound(position) {    
+function onLocationFound(position) {
     var radius = position.coords.accuracy / 2;
 
     marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-    circle = L.circle([position.coords.latitude, position.coords.longitude], radius).addTo(map); /*49.4874359,8.4614417*/
+    circle = L.circle([position.coords.latitude, position.coords.longitude], radius).addTo(map);
+    
+    centerMap();
 }
-
+    
 function onLocationUpdated (position) {
     map.removeLayer(marker);
     map.removeLayer(circle);
@@ -29,6 +50,10 @@ function onLocationUpdated (position) {
 
 function onLocationError(e) {
     alert(e.message);
+}
+
+function centerMap() {
+    map.setView(marker.getLatLng());
 }
 
 map.on('locationerror', onLocationError);
