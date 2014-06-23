bcbdaaa4df07d35d6d99d6a53ac41a7125f73a59
Marco Heumann <marco.heumann@web.de>
2014-06-23 11:49:49 +0200

    implemented new design and converted map to a js-object

--- js/map.js
+++ js/map.js
@@ -1,10 +1,59 @@
 var map = L.map('map');
-var marker, circle;
 
-L.tileLayer('img/map/{z}/{x}/{y}.jpg', {
-    minZoom: 18, // changed to 18 for debug because only zoom 18 exists (installls faster)
-    maxZoom: 18
-}).addTo(map);
+var mapControl = {
+    initialize: function() {
+        L.tileLayer('img/map/{z}/{x}/{y}.jpg', {
+            minZoom: 15,
+            maxZoom: 18
+        }).addTo(map);
+        
+        map.locate({setView: true});
+        
+        map.addControl(new CenterMapControl());
+        
+        $(".center-text").text("Zentrieren");
+        
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        map.on('locationerror', onLocationError);
+
+        document.addEventListener("pause", function () {navigator.geolocation.clearWatch(this.curPos);}, false);
+
+        document.addEventListener("resume", function () {watchPosition();}, false);
+    },
+
+    onLocationFound: function(position) {
+        this.radius = position.coords.accuracy / 2;
+
+        this.marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point", {'closeOnClick': false, 'closeButton': false}).openPopup();
+
+        this.circle = L.circle([position.coords.latitude, position.coords.longitude], radius).addTo(map);
+
+        this.centerMap();
+    },
+    
+    onLocationUpdated: function (position) {
+        map.removeLayer(this.marker);
+        map.removeLayer(this.circle);
+
+        this.onLocationFound(position);
+    },
+
+    onLocationError: function(e) {
+        alert(e.message);
+    },
+
+    centerMap: function() {
+        map.setView(this.marker.getLatLng());
+    },
+
+    watchPosition: function() {
+        this.curPos = navigator.geolocation.watchPosition(onLocationUpdated);
+    }
+};
 
 var CenterMapControl = L.Control.extend({
     options: {
@@ -21,47 +70,3 @@ var CenterMapControl = L.Control.extend({
         return container;
     }
 });
-
-map.addControl(new CenterMapControl());
-$(".center-text").text("Zentrieren");
-
-map.locate({setView: true});
-
-navigator.geolocation.getCurrentPosition(onLocationFound, onLocationError);
-var curPos = navigator.geolocation.watchPosition(onLocationUpdated);
-
-function onLocationFound(position) {
-    var radius = position.coords.accuracy / 2;
-
-    marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point", {'closeOnClick': false, 'closeButton': false}).openPopup();
-
-    circle = L.circle([position.coords.latitude, position.coords.longitude], radius).addTo(map);
-    
-    centerMap();
-}
-    
-function onLocationUpdated (position) {
-    map.removeLayer(marker);
-    map.removeLayer(circle);
-    
-    onLocationFound(position);
-}
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-function centerMap() {
-    map.setView(marker.getLatLng());
-}
-
-function watchPosition() {
-    curPos = navigator.geolocation.watchPosition(onLocationUpdated);
-}
-
-map.on('locationerror', onLocationError);
-
-document.addEventListener("pause", function () {navigator.geolocation.clearWatch(curPos);}, false);
-
-document.addEventListener("resume", function () {watchPosition();}, false);
