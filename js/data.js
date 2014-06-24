a60b4d370fe32cbff2650f5171b251e78a061931
MikeySkullivan <Cyberfreak963@web.de>
2014-06-24 14:30:13 +0200

    Format ausgebessert

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-var tour_data = {
+ï»¿var tour_data = {
     "id": 0,
     "name": "default",
     "lang": "de",
@@ -10,68 +10,68 @@ var tour_data = {
             "id": 0,
             "coordinates": {"x": 0, "y": 0},
             "title": "Das Mahnmal",
-			"intro": "Für jüdische Opfer des Nationalsozialismus wurde ein Mahnmal errichtet. Auf diesem wurden in Spiegelschrift mehr als 2200 Opfer eingraviert.",
+            "intro": "FÃ¼r jÃ¼dische Opfer des Nationalsozialismus wurde ein Mahnmal errichtet. Auf diesem wurden in Spiegelschrift mehr als 2200 Opfer eingraviert.",
             "question": "Wie viele Seiten des Mahnmals wurden beschrieben?",
             "solution": "4",
             "hint": "Das Mahnmal ist ein Kubus.",
             "nextid": 1
         },
-		1: {
+        1: {
             "id": 1,
             "coordinates": {"x": 0, "y": 0},
             "title": "Der Marktplatz",
-			"intro": "Der Götterbote Merkur, welcher auf dem Marktplatzbrunnen sogar die Stadtgöttin Mannhemia(Mannheimia)überragt, hält in der einen Hand den Hermesstab.",
-            "question": "Was hält er in der anderen Hand?",
+            "intro": "Der GÃ¶tterbote Merkur, welcher auf dem Marktplatzbrunnen sogar die StadtgÃ¶ttin Mannhemia(Mannheimia)Ã¼berragt, hÃ¤lt in der einen Hand den Hermesstab.",
+            "question": "Was hÃ¤lt er in der anderen Hand?",
             "solution": "Eine Sonne",
             "hint": "",
             "nextid": 2
         },
-		2: {
+        2: {
             "id": 2,
             "coordinates": {"x": 0, "y": 0},
             "title": "Der Friedensengel",
-			"intro": "",
+            "intro": "",
             "question": "Welcher Bundeskanzler weihte den Friedensengel 1952 ein?",
             "solution": "Konrad Adenauer",
             "hint": "Meist werden bedeutende Ereignisse fotographisch festgehalten.",
             "nextid": 3
         },
-		3: {
+        3: {
             "id": 3,
             "coordinates": {"x": 0, "y": 0},
             "title": "Die Hauptsynagoge",
-			"intro": "",
-            "question": "Welcher Art von Angriff zerstörte im Zweiten Weltkrieg neben Sprengladungen die Synagoge?",
+            "intro": "",
+            "question": "Welcher Art von Angriff zerstÃ¶rte im Zweiten Weltkrieg neben Sprengladungen die Synagoge?",
             "solution": "Ein Luftangriff",
             "hint": "",
             "nextid": 4
         },
-		4: {
+        4: {
             "id": 4,
             "coordinates": {"x": 0, "y": 0},
             "title": "Der Schillerplatz",
-			"intro": "",
-            "question": "Welches 1778 eröffnete Gebäude, das hier an diesem Ort stand, war ein Geschenk den Kurfürsten Karl Theodors, nachdem er Bayern erbte?",
+            "intro": "",
+            "question": "Welches 1778 erÃ¶ffnete GebÃ¤ude, das hier an diesem Ort stand, war ein Geschenk den KurfÃ¼rsten Karl Theodors, nachdem er Bayern erbte?",
             "solution": "Das Nationaltheater",
-            "hint": "Vor Ort befindet sich eine Tafel, auf der die Lösung zu finden ist.",
+            "hint": "Vor Ort befindet sich eine Tafel, auf der die LÃ¶sung zu finden ist.",
             "nextid": 5
         },
-		5: {
+        5: {
             "id": 5,
             "coordinates": {"x": 0, "y": 0},
             "title": "Der Wasserturm",
-			"intro": "Blickt man mittig auf der Aussichtsplattform des Wasserturms in Richtung der Augustanlage, ist eine Laterne zu sehen, deren Fuß scheinbar auf eine Skulptur zeigt.",
-            "question": "Welchen Titel trägt diese Skulpur?",
+            "intro": "Blickt man mittig auf der Aussichtsplattform des Wasserturms in Richtung der Augustanlage, ist eine Laterne zu sehen, deren FuÃŸ scheinbar auf eine Skulptur zeigt.",
+            "question": "Welchen Titel trÃ¤gt diese Skulpur?",
             "solution": "Das Rad",
             "hint": "Der Bildhauer war Morice Lipsi (Tafel vor Ort).",
             "nextid": 6
         },
-		6: {
+        6: {
             "id": 6,
             "coordinates": {"x": 0, "y": 0},
             "title": "Schloss Mannheim",
-			"intro": "",
-            "question": "Auf dem Schlossplatz sind zwei Skulpturen zu sehen (Karl Theodor und Karl Philipp). - Welches Gemeinsamkeit besteht zwischen der Skulptur Karl Theodors undeinem lokal populären Fußballclub des Stadtteils Waldhof?",
+            "intro": "",
+            "question": "Auf dem Schlossplatz sind zwei Skulpturen zu sehen (Karl Theodor und Karl Philipp). - Welches Gemeinsamkeit besteht zwischen der Skulptur Karl Theodors undeinem lokal populÃ¤ren FuÃŸballclub des Stadtteils Waldhof?",
             "solution": "",
             "hint": "",
             "nextid": 0
