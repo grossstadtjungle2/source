2f03cee6ab850fe3e4cbaa2b08434706158d41ad
hendrikniemann <h.neverman@web.de>
2014-09-19 19:53:46 +0200

    further developing on tour object and the tourdata

--- js/tour.js
+++ js/tour.js
@@ -35,7 +35,7 @@ app.save = {
 	 * Gibt das Starträtsel zurück
 	 * @Return {Number|Boolean}
 	 */
-	getStart: function () {
+	getStart: function() {
 		return window.localStorage['startQ'] || false;
 	},
 
@@ -65,34 +65,67 @@ app.save = {
 	/**
 	 * Löscht alle Tourdaten
 	 */
-	delete: function() {
-		window.localStorage.clear();
+	deleteTourData: function() {
+		delete window.localStorage['nextQ'];
+		delete window.localStorage['lastA'];
+		delete window.localStorage['startQ'];
+	}
+
+	/**
+	 * Gibt die festgelegte Sprache zurück
+	 * @return {String}
+	 */
+	getLang: function() {
+		return window.localStorage['lang'] || 'de';
+	}
+
+	/**
+	 * Setz die Sprache der der App auf einen Wert
+	 * @param {String} lang Die Sprache
+	 */
+	setLang: function( lang ) {
+		window.localStorage['lang'] = lang;
 	}
 };
 
 function Tour( data ) {
 	var self = this;
 
+	this.id = data.id || 0;
 	this.data = data;
 
 	if ( app.save.getTour() !== this.data.id ) {
 		app.save.delete();
 	}
 
+	this.currentQ = findStart();
 
+	this.enableNext
+
+	// TODO: Hier muss noch angepasst werden auf die neue Version von mapControl usw.
+	/**
+	 * Gibt das erste Rätsel zurück. Dabei wird entwerder auf die Savedata zurückgegriffen
+	 * oder der nächste Punkt über die Geolocation gefunden.
+	 * @return {Object}
+	 */
 	function findStart() {
+		var dist, start;
 		if ( !app.save.getStart() ) {
 			start = {distance: Infinity, id: -1};
-	        for (key in this.data.points) {
+	        for (key in self.data.points) {
 	            if (typeof key === 'undefined' || typeof key === 'function') {
 	                continue;	            	
 	            }
-	            dist = Math.pow(111.3 * (this.data.points[key].coords.lat - mapControl.curPos[0]), 2);
-	            dist += Math.pow(71.5 * (this.data.points[key].coords.lng - mapControl.curPos[1]), 2);
+	            dist = Math.pow(111.3 * (self.data.points[key].coords.lat - mapControl.curPos[0]), 2);
+	            dist += Math.pow(71.5 * (self.data.points[key].coords.lng - mapControl.curPos[1]), 2);
 	            dist = Math.sqrt(dist);
-	            if (dist < start.distance)
+	            if (dist < start.distance) {
 	                start = {distance: dist, id: key};
+	            }
 	        }
+	        return this.data.points[start.key];
+		} else {
+			return this.data.points[app.save.getStart()];
 		}
 	}
 }
